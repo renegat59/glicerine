737438ca887c619b1243f9cf2022f8a024b91c5b
Mateusz P <bananq@gmail.com>
2017-08-20 23:54:54 +0200

    test of cliparams'

--- src/core/CliParams.php
+++ src/core/CliParams.php
@@ -10,42 +10,45 @@ namespace Glicerine\core;
 
 class CliParams
 {
-    private $argv;
-    private $argc;
     private $cwd;
+    private $command = null;
+    private $action = null;
+    private $scriptName;
+    private $params = [];
     
     public function __construct($argv, $argc, $cwd)
     {
-        $this->argv = $argv;
-        $this->argc = $argc;
         $this->cwd = $cwd;
+        $this->prepareParams($argv, $argc);
     }
 
     public function getScriptName(): string
     {
-        return $this->argv[0];
+        return $this->scriptName;
     }
 
     public function getCommand()
     {
-        if($this->argc > 1){
-            $command = $this->argv[1];
-            if(0 !== strpos($command, '--')) {
-                return $command;
-            }
-        }
-        return null;
+        return $this->command;
+//        if($this->argc > 1){
+//            $command = $this->argv[1];
+//            if(0 !== strpos($command, '--')) {
+//                return $command;
+//            }
+//        }
+//        return null;
     }
 
     public function getAction()
     {
-        if($this->argc > 2){
-            $action = $this->argv[2];
-            if(0 !== strpos($action, '--')) {
-                return $action;
-            }
-        }
-        return null;
+        return $this->action;
+//        if($this->argc > 2){
+//            $action = $this->argv[2];
+//            if(0 !== strpos($action, '--')) {
+//                return $action;
+//            }
+//        }
+//        return null;
     }
 
     public function getCwd()
@@ -53,13 +56,29 @@ class CliParams
         return $this->cwd;
     }
 
-    public function getParam($param)
+    public function getParam(string $param)
     {
-        //TODO: Implement
+        return $this->params[$param] ?? null;
     }
 
     public function seatParam($param, $value)
     {
-        //TODO: Implement
+        $this->params[$param] = $value;
+    }
+
+    private function prepareParams($argv, $argc)
+    {
+        $this->scriptName = $argv[0];
+        for($index = 1; $index < $argc; $index++) {
+//            if($index == 1){
+//               $command = $argv[1];
+//               if(0 !== strpos($command, '--')) {
+//
+//               }
+//            }
+//            if(0 === strpos($arg, '--')) {
+//
+//            }
+        }
     }
 }
--- src/exceptions/InvalidConfigurationException.php
+++ src/exceptions/InvalidConfigurationException.php
@@ -1,9 +1,4 @@
 <?php
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 namespace Glicerine\exceptions;
 
@@ -14,5 +9,5 @@ namespace Glicerine\exceptions;
  */
 class InvalidConfigurationException extends GlicerineException
 {
-    //put your code here
+    
 }
