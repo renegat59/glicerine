bded848f75d845405835d00a7f48ee752834bfb5
Mateusz P <bananq@gmail.com>
2017-08-26 10:28:01 +0200

    added empty validators

---
+++ src/validators/DirectoryValidator.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Glicerine\validators;
+
+/**
+ * Description of DirectoryValidator
+ *
+ * @author Mateusz P <bananq@gmail.com>
+ */
+class DirectoryValidator extends Validator
+{
+    //put your code here
+    protected function validateParam()
+    {
+
+    }
+}
---
+++ src/validators/ListValidator.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Glicerine\validators;
+
+/**
+ * Description of ListValidator
+ *
+ * @author Mateusz P <bananq@gmail.com>
+ */
+class ListValidator extends Validator
+{
+    //put your code here
+    protected function validateParam()
+    {
+
+    }
+}
